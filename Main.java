5c821a644eb3b7860629a33ee5c56be6d0bec2b7
EternoDejaVu <105560004+EternoDejaVu@users.noreply.github.com>
2022-08-11 12:11:24 +0300

    Delete Main.java

--- Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        int cost = 2000;
-        int miles = cost / 20;
-        System.out.println("Миль за покупку:" + miles);
-    }
-}
